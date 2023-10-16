db619dc65441c9c1e4f54ab6265e48d1df361c6b
Ted Bell <tchbell@gmail.com>
2023-10-16 14:59:43 -0400

    added terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,6 +4,9 @@ import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import packageJson from './package.json' assert { type: 'json' };
 
+import { terser } from '@rollup/plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
 export default [
   {
     input: 'src/index.ts',
@@ -20,9 +23,11 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
+      terser(),
+      typescript({ tsconfig: './tsconfig.json', sourceMap: true }),
     ],
   },
   {
