cfbf963a21387337bb880914fc30b5effd577f92
unknown <tchbell@gmail.com>
2023-10-26 21:15:38 -0400

    update rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import packageJson from './package.json' assert { type: 'json' };
-
+import postcss from "rollup-plugin-postcss";
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
@@ -28,6 +28,7 @@ export default [
       commonjs(),
       terser(),
       typescript({ tsconfig: './tsconfig.json', sourceMap: true }),
+      postcss(), 
     ],
   },
   {
